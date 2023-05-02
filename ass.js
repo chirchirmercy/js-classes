43d3e143e48ffcbc609e216cd47a19e4d8d98c8a
chirchirmercy <cmercychptoo.com>
2023-05-02 12:55:42 +0300

    first commit

---
+++ ass.js
@@ -0,0 +1,35 @@
+
+
+//You are working on a web application for a car rental company. They want to keep track
+//of their car inventory and rental information. Your task is to create a JavaScript class or
+//function constructor for representing cars in their inventory and another one for
+//representing rental information.
+
+
+//Requirements:
+// 1) Create a Car class or function constructor that has the following properties:
+// a. make (string): The make of the car, e.g., "Toyota".
+// b. model (string): The model of the car, e.g., "Camry".
+// c. year (number): The year the car was manufactured, e.g., 2020.
+// d. isAvailable (boolean): Indicates if the car is currently available for rent.
+// The Car class or function constructor should also have a method called
+// toggleAvailability that changes the isAvailable property to its opposite value (true to
+// false, false to true)
+
+
+class Car{
+    constructor(make,model,year ){
+
+    this.model=model
+    this.make=model
+    this.year= year
+    this.isAvailable=this.isAvailable
+    }
+    toggledAvailability(){
+        this.toggledAvailability!==this.toggledAvailability
+    }
+   
+
+};
+console.log(car)
+let Car=newCar("Toyota","camry",2020)
